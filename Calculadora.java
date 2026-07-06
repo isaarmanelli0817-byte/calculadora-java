91bf05328fe1a6ab17877f7b875bf2769a48c830
armanelli-dev <isaarmanelli0817@gmail.com>
2026-07-06 18:39:25 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro número:");
+        double n1 = sc.nextDouble();
+        System.out.println("Digite o segundo número:");
+        double n2 = sc.nextDouble();
+        
+        System.out.println("Soma: " + (n1 + n2));
+        System.out.println("Subtração: " + (n1 - n2));
+        sc.close();
+    }
+}
